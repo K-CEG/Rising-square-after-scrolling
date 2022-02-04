3f3100d777f4566b3f2c20619447923ee9685588
K-CEG <krzysiek.cegielko@gmail.com>
2022-02-04 15:56:09 +0100

    Add new files

---
+++ main.js
@@ -0,0 +1,30 @@
+const square = document.createElement('div');
+document.body.appendChild(square);
+
+let grow = true;
+
+let size = 100;
+square.style.width = size +'px';
+square.style.height = size +'px';
+
+
+window.addEventListener("scroll", function(){
+    size += 10
+    if (grow == true){
+    size +=5
+    square.style.width = size + 'px';
+    square.style.height = size + 'px';
+    }
+    else {
+        size -= 5
+        square.style.width = size + 'px';
+        square.style.height = size + 'px';
+    }
+
+    if(size >= window.innerWidth/2){
+        grow = false
+    }
+    else if(size == 0) {
+        grow = true;
+    }
+})
